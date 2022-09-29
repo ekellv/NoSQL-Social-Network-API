55b767ff0bb8072b1fe2d849f6710bd6e4ba84d3
Erin V <erinkvoelker@gmail.com>
2022-09-29 17:09:47 -0400

    initial routing

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+// require express router
+const router = require('express').Router();
+
+// pulling in api routes 
+const apiRoutes = require('./api')
+
+// route prefix
+const apiRoutes = require('./api', apiRoutes);
+
+// 404 message if there's an error in routing 
+router.use((req,res) => {
+    res.status(404).send('<h1>404 Error! Please try again!</h1>'); 
+});
+
+// exporting router 
+module.exports = router; 
