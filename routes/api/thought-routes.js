41f9533ce66d83af1cf9e7bfaf933453bc625d84
Erin V <erinkvoelker@gmail.com>
2022-09-29 16:45:47 -0400

    created initial thought routes

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,35 @@
+// requiring the express router
+const router = require('express').Router;
+
+// importing Thought controller functions 
+
+const { 
+    getThoughts,
+    getThoughtbyId,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
+} = require('../../controllers/thought-controller');
+const { create } = require('../../models/User');
+
+// Thought Routes //
+
+// (GET) path: localhost:3001/api/thoughts
+router.route('/').get(getThoughts);
+
+// (GET)(PUT)(DELETE) path: localhost:3001/thoughts/:id
+router.route('/:id').get(getThoughtbyId).put(updateThought).delete(deleteThought);
+
+// (POST) path: localhost:3001/api/thoughts/:userId
+router.route('/:userId').post(createThought);
+
+// (POST) path: localhost:3001/api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(addReaction);
+
+// (DELETE) path: localhost:3001/api/thoughts/:thoughtId/reactionId
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
+
+// export thought routes 
+module.exports = router; 
