6c324c7080d9389452df14c692df84663fcc0ce9
Erin V <erinkvoelker@gmail.com>
2022-09-29 17:09:36 -0400

    initial api routes

---
+++ routes/api/index.js
@@ -0,0 +1,13 @@
+// express router
+const router = require('express').Router();
+
+// pulling in Thought & User routes 
+const thoughtRoutes = require('./thought-routes');
+const userRoutes = require('./user-routes');
+
+// route prefixes 
+router.use('/thoughts', thoughtRoutes);
+router.user('/users', userRoutes);
+
+// exporting routes
+module.exports = router; 
